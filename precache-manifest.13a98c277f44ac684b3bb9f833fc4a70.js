774292568265f1297382fc5c675a88a00e948106
Lekan Ojulowo <lekanojulowo@gmail.com>
2021-05-12 15:25:02 +0100

    initial deployment

---
+++ precache-manifest.13a98c277f44ac684b3bb9f833fc4a70.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a81df7b3d0b85e47548",
+    "url": "/css/about.73199d59.css"
+  },
+  {
+    "revision": "0b75d1eb278c2f621cd8",
+    "url": "/css/app.4d24365d.css"
+  },
+  {
+    "revision": "a1bc8b1f8e334299e204",
+    "url": "/css/chunk-vendors.1b81f9d5.css"
+  },
+  {
+    "revision": "082a55033fe61f2c4142",
+    "url": "/css/contact.b4a4224e.css"
+  },
+  {
+    "revision": "d5c2f2301c93223ebe7a2e7ef65b011f",
+    "url": "/imagez7.jpg"
+  },
+  {
+    "revision": "a833110ca87549b31e592d0de2d789c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a81df7b3d0b85e47548",
+    "url": "/js/about.e418e256.js"
+  },
+  {
+    "revision": "0b75d1eb278c2f621cd8",
+    "url": "/js/app.330a8080.js"
+  },
+  {
+    "revision": "a1bc8b1f8e334299e204",
+    "url": "/js/chunk-vendors.0427f0c7.js"
+  },
+  {
+    "revision": "082a55033fe61f2c4142",
+    "url": "/js/contact.ee6ea811.js"
+  },
+  {
+    "revision": "eefd8da330e90f41ba164a8985aa33df",
+    "url": "/logo.jpg"
+  },
+  {
+    "revision": "330c8c961fc6cf86ef0a7ffb24bd62e2",
+    "url": "/logoo.png"
+  },
+  {
+    "revision": "2d2dcc695cf43352b420cdd8e8b1a43c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
